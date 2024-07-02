f1b338fe93e5d979fa19a239ee875c942f61a682
chr1swill <alexanderwilliams20003@gmail.com>
2024-07-01 23:06:06 -0400

    feat: remove audio player code

--- src/js/app.js
+++ src/js/app.js
@@ -14,7 +14,7 @@ try {
         fetch(`http://localhost:3000?url=${urlToFetch}`)
             .then(function(response) {
                 if (!response.ok) {
-                    throw new Error(`Failed to fetch data from endpoint: ${searchBar.value.trim()}`);
+                    throw new Error(`Failed to fetch data from endpoint: ${urlToFetch}`);
                 }
 
                 return response.text()
@@ -26,62 +26,8 @@ try {
             })
             .then(function(body) {
                 console.assert("There was not body text return form the fetch", body);
-                let chars = 0;
                 const arr = body.split("")
-                while (chars < arr.length) {
-                    chars++;
-                }
-
-                console.log("total num of chars: ", chars);
-
-                const synth = window.speechSynthesis;
-                const utterThis = new SpeechSynthesisUtterance(body);
-
-                synth.onvoiceschanged = () => {
-                    const voices = synth.getVoices();
-                    if (voices.length > 0) {
-                        utterThis.voice = voices[0];
-                        utterThis.pitch = '1';
-                        utterThis.rate = '1';
-
-                        console.log("text is being spoken 1: ", synth.speaking);
-                        synth.speak(utterThis);
-                        console.log("text is being spoken 2: ", synth.speaking);
-
-                        utterThis.onend = function() {
-                            console.log("utter ended");
-                        };
-
-                        utterThis.onpause = function() {
-                            console.log("utter pause");
-                        };
-
-                        utterThis.onstart = function() {
-                            console.log("utter started");
-                        };
-
-                        utterThis.onresume = function() {
-                            console.log("utter resumed");
-                        };
-
-                        utterThis.onerror = function() {
-                            console.error("utter errored");
-                        };
-
-                        utterThis.onmark = function() {
-                            console.log("utter marked or whatever that means");
-                        };
-                    } else {
-                        console.error("No voices available");
-                    }
-                };
-                synth.onvoiceschanged();
-
-                // Ensure the voices are loaded
-                if (synth.getVoices().length > 0) {
-                    console.log("running");
-                    synth.onvoiceschanged();
-                }
+                console.log("Charater count: ", arr.length);
             })
             .catch(function(err) {
                 console.error(err);
