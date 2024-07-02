92df34d618f7c6a9434a7c0de4e39559beabb723
chr1swill <alexanderwilliams20003@gmail.com>
2024-07-02 11:43:48 -0400

    feat: handling form submittion

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import fs from 'node:fs';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import ejs from 'ejs';
+import { JSDOM } from 'jsdom';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -46,6 +47,19 @@ function serverStaticFile(res, filePath, contentType) {
     });
 }
 
+/**
+ * @param{string} url
+ * @returns{boolean} 
+ */
+function isValidUrl(url) {
+    try {
+        new URL(url);
+        return true;
+    } catch (_) {
+        return false;
+    }
+}
+
 const server = http.createServer(function(req, res) {
     const reqUrl = new URL(req.url, `http://${req.headers.host}`);
     const targetUrl = reqUrl.searchParams.get('url');
@@ -66,6 +80,100 @@ const server = http.createServer(function(req, res) {
             res.end(content);
         });
         return;
+    }
+
+    if (reqUrl.pathname === '/submit-url') {
+        if (req.method !== 'POST') {
+            res.writeHead(405, { 'Content-Type': 'text/plain' });
+            res.end('Method Not Allowed');
+            return;
+        }
+
+        let body = ''
+
+        req.on('data', function(chunk) {
+            body += chunk.toString();
+        })
+
+        req.on('end', function() {
+            const parsedBody = new URLSearchParams(body);
+
+            /**
+             *@typedef {Object} SubmitUrlFormData
+             *@property {string} q - a url to handle or empty string 
+             */
+
+            /**@type{SubmitUrlFormData}*/
+            const formData = Object.fromEntries(parsedBody.entries());
+
+            console.log("submitted form data: \n", formData);
+
+            if (!formData.q || !isValidUrl(formData.q) || formData.q.trim() === '') {
+                res.writeHead(400, { 'Content-Type': 'text/plain' });
+                res.end("Invalid URL submitted");
+                return;
+            }
+
+            fetch(formData.q)
+                .then(function(response) {
+                    if (!response.ok) {
+                        throw new Error("Failed to fetch data form provided endpoint: ", formData.q);
+                    }
+
+                    return response.text();
+                })
+                .then(function(html) {
+                    const dom = new JSDOM(html);
+                    const document = dom.window.document;
+                    return document.body.textContent;
+                })
+                .then(function(textContent) {
+                    if (textContent === null || textContent.trim() === '') {
+                        throw new Error("Parsing the body of the fetch document yeilded no text content: ", formData.q);
+                    }
+
+                    // do db stuff
+
+                    const data = { text: textContent };
+
+                    ejs.renderFile(path.join(__dirname, "src", "templates", "views", "text-reader.ejs"), data, function(err, str) {
+                        if (err) {
+                            res.writeHead(500, { 'Content-Type': 'text/plain' });
+                            res.end('Internal Server Error');
+                            return;
+                        }
+
+                        if (!res.headersSent) {
+                            res.writeHead(200, { 'Content-Type': 'text/html' });
+                        }
+
+                        res.end(str);
+                    })
+                })
+                .catch(function({ name, message }) {
+                    console.error(err);
+                    const error = { error_message: `${name}: ${message}` };
+                    ejs.renderFile(path.join(__dirname, "src", "templates", "views", "error.ejs"), error, function(err, str) {
+                        if (err) {
+                            res.writeHead(500, { 'Content-Type': 'text/plain' });
+                            res.end('Internal Server Error');
+                            return;
+                        }
+
+                        if (!res.headersSent) {
+                            res.writeHead(400, { 'Content-Type': 'text/html' });
+                        }
+
+                        res.end(str);
+                    });
+
+                    return;
+                })
+
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('Form data submittied successfully');
+            return;
+        })
     }
 
     if (reqUrl.pathname === '/app.js') {
