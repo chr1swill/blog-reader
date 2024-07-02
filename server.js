5d922b09e341fb19b1c957d1016ba397d9bd7a51
chr1swill <alexanderwilliams20003@gmail.com>
2024-07-02 17:22:56 -0400

    feat: handle form submition as a get req

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import { JSDOM } from 'jsdom';
 import sqlite3 from 'sqlite3';
 
 const sqlite3Verbose = sqlite3.verbose();
-const db = new sqlite3Verbose.Database(':memory:', function(err) {
+const db = new sqlite3Verbose.Database('./db/test.db', function(err) {
     if (err) {
         console.error(err.message);
     }
@@ -16,46 +16,22 @@ const db = new sqlite3Verbose.Database(':memory:', function(err) {
     console.log("Connected to the SQLite database");
 });
 
-db.serialize(function() {
-    db.run(`CREATE TABLE IF NOT EXIST url (
+db.run(`CREATE TABLE IF NOT EXIST url (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         path TEXT NOT NULL, 
         text_content TEXT NOT NULL
         );`, function(err) {
 
-        if (err) {
-            console.error(err.message);
-        }
+    if (err) {
+        console.error(err.message);
+        return;
+    }
 
-        console.log("Table created or already exists.");
-    });
-})
+    console.log("Table created or already exists.");
+});
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-function fetchContent(targetUrl, callback) {
-    const parseUrl = new URL(targetUrl);
-    const protocol = parseUrl.protocol === 'https:' ? https : http;
-
-    protocol.get(targetUrl, function(response) {
-        let data = ''
-
-        response.on('data', function(chunk) {
-            data += chunk;
-        });
-
-        response.on('end', function() {
-            callback(null, data);
-        });
-
-        response.on('error', function(err) {
-            callback(err);
-        });
-    }).on('error', function(err) {
-        callback(err);
-    });
-}
 
 function serverStaticFile(res, filePath, contentType) {
     fs.readFile(filePath, function(err, data) {
@@ -87,115 +63,108 @@ function isValidUrl(url) {
 
 const server = http.createServer(function(req, res) {
     const reqUrl = new URL(req.url, `http://${req.headers.host}`);
-    const targetUrl = reqUrl.searchParams.get('url');
-
-    if (targetUrl !== null) {
-        fetchContent(targetUrl, function(err, content) {
-            if (err) {
-                if (!res.headersSent) {
-                    res.writeHead(500, { 'Content-Type': 'text/plain' });
-                }
-                res.end('Error fetching the Url');
-                return;
-            }
-
-            if (!res.headersSent) {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-            }
-            res.end(content);
-        });
-        return;
-    }
 
     if (reqUrl.pathname === '/submit-url') {
-        if (req.method !== 'POST') {
+        if (req.method !== 'GET') {
             res.writeHead(405, { 'Content-Type': 'text/plain' });
             res.end('Method Not Allowed');
             return;
         }
 
-        let body = ''
+        const query = reqUrl.searchParams;
 
-        req.on('data', function(chunk) {
-            body += chunk.toString();
-        })
+        /**
+         *@typedef {Object} SubmitUrlFormData
+         *@property {string} q - a url to handle or empty string 
+         */
 
-        req.on('end', function() {
-            const parsedBody = new URLSearchParams(body);
+        /**@type{SubmitUrlFormData}*/
+        const formData = { q: query.get('q') };
+        console.log("submitted form data: \n", formData);
 
-            /**
-             *@typedef {Object} SubmitUrlFormData
-             *@property {string} q - a url to handle or empty string 
-             */
+        if (formData.q === null || !isValidUrl(formData.q) || formData.q.trim() === '') {
+            console.warn("Form was submitted with an empty query");
+            res.writeHead(204, { 'Content-Type': 'text/plain' });
 
-            /**@type{SubmitUrlFormData}*/
-            const formData = Object.fromEntries(parsedBody.entries());
-
-            console.log("submitted form data: \n", formData);
-
-            if (!formData.q || !isValidUrl(formData.q) || formData.q.trim() === '') {
-                res.writeHead(400, { 'Content-Type': 'text/plain' });
-                res.end("Invalid URL submitted");
-                return;
-            }
-
-            fetch(formData.q)
-                .then(function(response) {
-                    if (!response.ok) {
-                        throw new Error("Failed to fetch data form provided endpoint: ", formData.q);
-                    }
-
-                    return response.text();
-                })
-                .then(function(html) {
-                    const dom = new JSDOM(html);
-                    const document = dom.window.document;
-                    return document.body.textContent;
-                })
-                .then(function(textContent) {
-                    if (textContent === null || textContent.trim() === '') {
-                        throw new Error("Parsing the body of the fetch document yeilded no text content: ", formData.q);
-                    }
-
-                    // do db stuff
-                    db.
-
-                    const data = { text: textContent };
-
-                    ejs.renderFile(path.join(__dirname, "src", "templates", "views", "text-reader.ejs"), data, function(err, str) {
-                        if (err) {
-                            res.writeHead(500, { 'Content-Type': 'text/plain' });
-                            res.end('Internal Server Error');
-                            return;
-                        }
-
-                        if (!res.headersSent) {
-                            res.writeHead(200, { 'Content-Type': 'text/html' });
-                        }
-
-                        res.end(str);
-                    })
-                })
-                .catch(function({ name, message }) {
-                    console.error(err);
-                    const error = { error_message: `${name}: ${message}` };
-                    ejs.renderFile(path.join(__dirname, "src", "templates", "views", "error.ejs"), error, function(err, str) {
-                        if (err) {
-                            res.writeHead(500, { 'Content-Type': 'text/plain' });
-                            res.end('Internal Server Error');
-                            return;
-                        }
-
-                        if (!res.headersSent) {
-                            res.writeHead(400, { 'Content-Type': 'text/html' });
-                        }
-
-                        res.end(str);
-                    });
-
+            ejs.renderFile(path.join(__dirname, "src", "templates", "views", "index.ejs"), function(err, str) {
+                if (err) {
+                    res.writeHead(500, { 'Content-Type': 'text/plain' });
+                    res.end('Internal Server Error');
                     return;
+                }
+
+                if (!res.headersSent) {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                }
+
+                res.end(str);
+            })
+
+            return;
+        }
+
+        fetch(formData.q)
+            .then(function(response) {
+                if (!response.ok) {
+                    throw new Error("Failed to fetch data form provided endpoint: ", formData.q);
+                }
+
+                return response.text();
+            })
+            .then(function(html) {
+                const dom = new JSDOM(html);
+                const document = dom.window.document;
+                return document.body.textContent;
+            })
+            .then(function(textContent) {
+                if (textContent === null || textContent.trim() === '') {
+                    throw new Error("Parsing the body of the fetch document yeilded no text content: ", formData.q);
+                }
+
+                db.run(`INSERT INTO url (path, text_content) VALUES (${formData.q}, ${textContent});`, function(err) {
+                    if (err) {
+                        console.error(err.message);
+                        return;
+                    }
+
+                    console.log("Sucessfully inserted values for path and text content into db");
                 })
-        })
+
+                const data = { url: formData.q, text: textContent };
+
+                ejs.renderFile(path.join(__dirname, "src", "templates", "views", "text-reader.ejs"), data, function(err, str) {
+                    if (err) {
+                        res.writeHead(500, { 'Content-Type': 'text/plain' });
+                        res.end('Internal Server Error');
+                        return;
+                    }
+
+                    if (!res.headersSent) {
+                        res.writeHead(200, { 'Content-Type': 'text/html' });
+                    }
+
+                    res.end(str);
+                })
+            })
+            .catch(function({ name, message }) {
+                console.error(err);
+                const error = { error_message: `${name}: ${message}` };
+                ejs.renderFile(path.join(__dirname, "src", "templates", "views", "error.ejs"), error, function(err, str) {
+                    if (err) {
+                        res.writeHead(500, { 'Content-Type': 'text/plain' });
+                        res.end('Internal Server Error');
+                        return;
+                    }
+
+                    if (!res.headersSent) {
+                        res.writeHead(400, { 'Content-Type': 'text/html' });
+                    }
+
+                    res.end(str);
+                });
+
+                return;
+            })
     }
 
     if (reqUrl.pathname === '/text-reader') {
